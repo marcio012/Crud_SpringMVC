a95df9df122416e609dec9e968d21a5c500623e6
marcio heleno <marcio012@edu.unifor.br>
2018-08-30 15:20:42 -0300

    Gerando o primeiro commit do spring mvc

---
+++ src/main/java/br/com/marcioheleno/AppMasterSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package br.com.marcioheleno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMasterSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMasterSpringMvcApplication.class, args);
+	}
+}
